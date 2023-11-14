368d0f700e632f7bcf3697abc07c59876927c716
vcioffi <v.cioffi94@gmail.com>
2023-11-14 23:22:11 +0100

    Implemente Switch

--- app/src/main/java/com/cioffi/nfctoogle/MainActivity.kt
+++ app/src/main/java/com/cioffi/nfctoogle/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.cioffi.nfctoogle
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,25 +22,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Preview()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     NFCToogleTheme {
-        Greeting("Android")
+        ToogleNFCWidget().MyContent()
     }
 }
--- app/src/main/java/com/cioffi/nfctoogle/ToogleNFCWidget.kt
+++ app/src/main/java/com/cioffi/nfctoogle/ToogleNFCWidget.kt
@@ -1,13 +1,16 @@
 package com.cioffi.nfctoogle
 
 import android.content.Context
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
-import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.layout.Alignment
@@ -17,34 +20,39 @@ import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
 
+
 class ToogleNFCWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
 
         provideContent {
-            MyContent()
+                MyContent()
         }
     }
 
-    @Preview
+    //@Preview(showBackground = true, showSystemUi = true)
     @Composable
-    private fun MyContent() {
-        Column(
-            modifier = GlanceModifier.fillMaxSize(),
-            verticalAlignment = Alignment.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Where to?", modifier = GlanceModifier.padding(12.dp))
-            Row(horizontalAlignment = Alignment.CenterHorizontally) {
-                Button(
-                    text = "Home",
-                    onClick = actionStartActivity<MainActivity>()
-                )
-                Button(
-                    text = "Work",
-                    onClick = actionStartActivity<MainActivity>()
-                )
-            }
-        }
+     fun MyContent() {
+        //var checked by remember { mutableStateOf(true) }
+                Column(
+                    modifier = GlanceModifier.fillMaxSize(),
+                    verticalAlignment = Alignment.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "NFC", modifier = GlanceModifier.padding(12.dp))
+                    Row(horizontalAlignment = Alignment.CenterHorizontally) {
+
+                        var checked by remember { mutableStateOf(true) }
+
+/*                        Switch(
+                            checked = checked,
+                            onCheckedChange = {
+                                checked = it
+                            }
+                        )*/
+
+
+                    }
+                }
     }
 }

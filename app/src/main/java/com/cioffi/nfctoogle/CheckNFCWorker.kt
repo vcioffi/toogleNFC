db41c521f16dc450083ffeda2a622adfb4a719ad
vcioffi <v.cioffi94@gmail.com>
2024-01-15 14:39:05 +0100

    Notification

--- app/src/main/java/com/cioffi/nfctoogle/CheckNFCWorker.kt
+++ app/src/main/java/com/cioffi/nfctoogle/CheckNFCWorker.kt
@@ -25,6 +25,7 @@ private const val  TAG = "Test Worker"
 
 
     override suspend fun doWork(): Result {
+
         val nfcAdpt = NfcAdapter.getDefaultAdapter(context)
         Log.v(TAG, "nfcAdpt:  ${nfcAdpt}")
         var isNFCOn by mutableStateOf(nfcAdpt.isEnabled())
--- app/src/main/java/com/cioffi/nfctoogle/utils/WorkeUtils.kt
+++ app/src/main/java/com/cioffi/nfctoogle/utils/WorkeUtils.kt
@@ -3,21 +3,32 @@ package com.cioffi.nfctoogle.utils
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import android.provider.Settings
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.cioffi.nfctoogle.R
 
+
 fun makeStatusNotification(message: String, context: Context) {
+
+    //intent for notification action
+    val intentNfcAcxtion = Intent(Settings.ACTION_NFC_SETTINGS)
+    intentNfcAcxtion.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    var pintentNfcAcxtion = PendingIntent.getBroadcast(context, 0, intentNfcAcxtion,
+        PendingIntent.FLAG_IMMUTABLE)
 
     // Make a channel if necessary
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
-        val name =   "NFC toogle"
+        val name =   "NFC"
         val description = "Shows notifications whenever work starts"
         val importance = NotificationManager.IMPORTANCE_HIGH
         val channel = NotificationChannel("CHANNEL_ID", name, importance)
@@ -32,11 +43,12 @@ fun makeStatusNotification(message: String, context: Context) {
 
     // Create the notification
     val builder = NotificationCompat.Builder(context, "CHANNEL_ID")
-        .setSmallIcon(R.drawable.ic_launcher_foreground)
-        .setContentTitle("WorkRequest Starting")
+        .setSmallIcon(R.drawable.nfc_icon)
+        .setContentTitle("NFC")
         .setContentText(message)
         .setPriority(NotificationCompat.PRIORITY_HIGH)
         .setVibrate(LongArray(0))
+        .addAction(R.drawable.nfc_icon, "Turn OFF driving mode", pintentNfcAcxtion)
 
     // Show the notification
     if (ActivityCompat.checkSelfPermission(
@@ -51,6 +63,7 @@ fun makeStatusNotification(message: String, context: Context) {
         //                                          int[] grantResults)
         // to handle the case where the user grants the permission. See the documentation
         // for ActivityCompat#requestPermissions for more details.
+        Log.v("Test ", "No notification permission")
         return
     }
     NotificationManagerCompat.from(context).notify(1, builder.build())
